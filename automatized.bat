542bfe70c07d1f497e8c686fddf5bddfebec021d
CybeRooT-01 <birahim267@gmail.com>
2023-07-30 15:17:40 +0000

    add frontworkspace to the automatization

--- automatized.bat
+++ automatized.bat
@@ -2,6 +2,8 @@
 start /min "" cmd /k "/apache24/bin/httpd.exe"
 start /min cmd /k "cd C:\Users\CybeRooT\Desktop\Sonatel Academy\transfert\transfert-backend && code . &&exit"
 start /min "" cmd /k "cd C:\Users\CybeRooT\Desktop\Sonatel Academy\transfert\transfert-backend && php artisan serve"
+start /min "" cmd /k "cd C:\Users\CybeRooT\Desktop\Sonatel Academy\transfert\transfert-frontEnd && npx lite-server"
+start /min "" cmd /k "cd C:\Users\CybeRooT\Desktop\Sonatel Academy\transfert\transfert-frontEnd && code . && exit"
 start /min "" cmd /k "cd C:\Users\CybeRooT\Desktop && start postman && exit"
 start cmd /k "cd C:\Users\CybeRooT\Desktop\Sonatel Academy\transfert\transfert-backend"
 
